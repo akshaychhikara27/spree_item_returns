784d046d89da559bc96f67ac8a98f21571915c42
akshaychhikara27 <akshay.chhikara@intelligent.com>
2018-01-08 11:26:47 +0530

    try slim in deface

--- app/overrides/spree/return_authorization/user_return_authorization_history_button.rb
+++ app/overrides/spree/return_authorization/user_return_authorization_history_button.rb
@@ -2,8 +2,8 @@ Deface::Override.new(
   virtual_path: 'spree/users/show',
   name: 'user_return_authorization_history_button',
   insert_bottom: "[data-hook='account_my_orders'] h3",
-  text: "<%= link_to(spree.return_authorizations_path, class: 'btn btn-primary pull-right') do %>
-          <span class='glyphicon glyphicon-list-alt'></span>
-          <%= Spree.t(:returns_history) %>
-        <% end %>"
+  text: "= link_to(spree.return_authorizations_path, class: 'btn btn-primary pull-right') do
+          span.glyphicon.glyphicon-list-alt
+          = Spree.t(:returns_history)
+         "
 )
